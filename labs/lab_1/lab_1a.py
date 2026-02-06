ea909075d521953f6e5abe6fab25335f564bbc8a
khuang2028 <kevinhuang118559@gmail.com>
2026-02-05 22:10:40 -0500

    Added my introduction to lab_1a.py
    I introduced myself as well as stated why I joined this course and my hopes for future.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -10,8 +10,10 @@ def main():
 
     name = "" # TODO: Insert your name between the double quotes
     name = "Kevin"
-
+    introduction = "This is my introduction: "
     print(f"{name}, Welcome to the CSS course!")
+
+    print(introduction + "Hi! My name is Kevin, and I am a sophomore interested in robotics as well as video editing. I am starting this course because I want to leran more about version control and to be able to hopefully join the Underwater Autonomous Vehicle Challenge BWSI summer program!")
 
 if __name__ == "__main__":
     main()
